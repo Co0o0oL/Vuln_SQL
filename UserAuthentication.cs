597e57d70391b49f541820e03bcec25ef2f566c1
Co0o0oL <157955516+Co0o0oL@users.noreply.github.com>
2024-07-22 21:09:58 +0300

    Update UserAuthentication.cs

--- UserAuthentication.cs
+++ UserAuthentication.cs
@@ -1,1 +1,22 @@
+// Extracting user input from a POST request
+string username = Request.Form["username"];
+string password = Request.Form["password"];
+string rememberMe = Request.Form["rememberMe"];
 
+// Constructing a query to authenticate the user
+string query = "SELECT * FROM Users WHERE Username = '" + username + "' AND Password = '" + password + "'";
+query += " AND RememberMe = '" + rememberMe + "' AND Active = 1";
+
+// Preparing and executing the SQL command
+using (SqlConnection connection = new SqlConnection(connectionString))
+{
+    SqlCommand cmd = new SqlCommand(query, connection);
+    connection.Open();
+    SqlDataReader reader = cmd.ExecuteReader();
+
+    // Process the results
+    while (reader.Read())
+    {
+        // Logic to handle user data
+    }
+}
